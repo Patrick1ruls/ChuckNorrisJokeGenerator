9ebe2bfa984fdef18a5b97968f8711b674af2c97
Patrick1ruls <patrickvermillion555@gmail.com>
2019-09-18 14:00:47 -0500

    Adding Request Mapping

--- src/main/java/guru/springframework/chucknorrisforactuator/controllers/JokeController.java
+++ src/main/java/guru/springframework/chucknorrisforactuator/controllers/JokeController.java
@@ -4,6 +4,8 @@ package guru.springframework.chucknorrisforactuator.controllers;
 import guru.springframework.chucknorrisforactuator.services.JokeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JokeController {
@@ -13,5 +15,13 @@ public class JokeController {
     @Autowired
     public JokeController(JokeService jokeService) {
         this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chucknorris";
     }
 }
